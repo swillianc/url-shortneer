59087a078a85267793398e15f115455ff223b289
Willian Couto da Silva <s.willianc@gmail.com>
2023-05-31 10:41:03 -0300

    first commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: "http://localhost:3000",
+    MONGO_CONNECTION: 'mongodb+srv://sixxy:ZepHYR14@w!@url-shortneer.gws2hsc.mongodb.net/?retryWrites=true&w=majority'
+}
